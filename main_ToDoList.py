cb080fe431259d52d76cb2b0a47a1e32079c7023
tjb36 <43373700+tjb36@users.noreply.github.com>
2021-09-14 22:59:52 +0100

    Added all PyQt import statements

--- main_ToDoList.py
+++ main_ToDoList.py
@@ -1,3 +1,7 @@
 import sys
 import json
 import PyQt5
+
+from PyQt5.QtCore import QSize, Qt, QAbstractListModel
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QVBoxLayout,QHBoxLayout, QLineEdit, QListView
+from PyQt5.QtGui import QColor, QPalette
