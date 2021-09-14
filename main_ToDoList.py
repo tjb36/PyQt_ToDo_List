5d9d164b437fa7c718b5e8f6e8a47bd674d86106
tjb36 <43373700+tjb36@users.noreply.github.com>
2021-09-14 23:39:22 +0100

    Added the main application event loop

--- main_ToDoList.py
+++ main_ToDoList.py
@@ -5,3 +5,17 @@ import PyQt5
 from PyQt5.QtCore import QSize, Qt, QAbstractListModel
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QVBoxLayout,QHBoxLayout, QLineEdit, QListView
 from PyQt5.QtGui import QColor, QPalette
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("ToDo List")
+
+# Main application loop
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setStyle("Windows")
+    window = MainWindow()
+    window.show()
+    sys.exit( app.exec_() )
